e26ac7e6bfb7151276ad528609e9fdc681d7df5c
Jemesson Lima <jemessonlima@gmail.com>
2022-07-21 08:46:41 -0300

    Add copilot configs

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const { response } = require('express')
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    const msg = 'Node app Running AWS ECS Fargate'
+app.get('/', (_req, res) => {
+    const msg = 'Node app is Running on AWS'
     
     res.end(JSON.stringify({ message: msg }))
 })
 
 app.listen(3000, () => {
-    console.log(`server connected`)
+    console.log(`server connected!`)
 })
